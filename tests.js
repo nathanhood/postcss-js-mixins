16eca7912e88be4f4f9313c8557f98cbce756824
Nathan Hood <nathanhood@me.com>
2016-12-21 12:03:35 -0600

    Update tests with extended parser/stringifier

--- tests.js
+++ tests.js
@@ -1,9 +1,13 @@
 const expect = require('chai').expect;
 const postcss = require('postcss');
 const plugin = require('./index.js');
+const stringify = require('./lib/parse');
 
 function process(input, expected, opts = {}, warnings = 0) {
-	return postcss([ plugin(opts) ]).process(input)
+	return postcss([ plugin(opts) ]).process(input, {
+			parser: parser,
+			stringifier: stringify
+		})
 		.then(result => {
 			expect(result.css).to.equal(expected);
 			expect(result.warnings().length).to.equal(warnings);
