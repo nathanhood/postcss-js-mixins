f1a4d14d8b428c9e49877c38158c3dd1950c88b0
Nathan Hood <nathanhood@me.com>
2016-12-21 12:06:39 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const postcss = require('postcss');
 
+module.exports = postcss.plugin('postcss-mixins', (opts = {}) => {
+	return (root, result) => {
+		root.walk(node => {
+			// TODO: add new declaration and pass properties from mixin to declaration
+			// TODO: remove mixin
+			// let decl = new Declaration;
+
+			if (node.type === 'mixin') {
+
+			}
+		});
+	};
+});
