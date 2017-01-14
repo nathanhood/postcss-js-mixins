5bfda396eb25a72a376d7b4fd68586f0cf5b6dac
Nathan Hood <nathanhood@me.com>
2017-01-14 12:15:46 -0600

    Temporarily take out comment from parser

--- parser/Parser.js
+++ parser/Parser.js
@@ -27,32 +27,6 @@ function convertToObject(args) {
 }
 
 class Parser extends BaseParser {
-	comment(token) {
-		let node = new Comment();
-
-		this.init(node, token[2], token[3]);
-
-		node.source.end = { line: token[4], column: token[5] };
-
-		let text = token[1].slice(2);
-
-		if (text.slice(-2) === '*/') {
-			text = text.slice(0, -2);
-		}
-
-		if (/^\s*$/.test(text)) {
-			node.text = '';
-			node.raws.left = text;
-			node.raws.right = '';
-		} else {
-			let match = text.match(/^(\s*)([^]*[^\s])(\s*)$/);
-
-			node.text = match[2];
-			node.raws.left = match[1];
-			node.raws.right = match[3];
-		}
-	}
-
 	other() {
 		var token = void 0;
 		var end = false;
